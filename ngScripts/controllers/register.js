02c6e310a3ea3991d90164570a7b0218b172e9f1
Abhijitno1 <abhijitno1@yahoo.com>
2017-03-06 06:36:19 +0530

    clien side validation WIP

--- ngScripts/controllers/register.js
+++ ngScripts/controllers/register.js
@@ -1,3 +1,7 @@
 ﻿angular.module('AngularAuth').controller('register', function ($scope) {
     console.log('register controller loaded');
+
+    $scope.registerUser = function () {
+        alert('user registered');
+    };
 });
